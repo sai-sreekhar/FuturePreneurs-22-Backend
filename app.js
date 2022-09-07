4d5bd833e93b8fb876d85299855b285bfc5f2935
Sai Sreekar <sai.sreekhar@gmail.com>
2022-09-08 02:19:50 +0530

    added morgan logs

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const cors = require("cors");
 const errorController = require("./controllers/errorController");
 const { errorCodes } = require("./utils/constants");
 const AppError = require("./utils/appError");
+const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
 
 app.use(cors());
+app.use(morgan("combined"));
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/team", require("./routes/teamRoutes"));
