93a0c48a42c4f450a6127899ea19851467fe838b
Sai Sreekar <sai.sreekhar@gmail.com>
2022-09-08 23:17:02 +0530

    modified cors

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ const morgan = require("morgan");
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 app.use(morgan("dev"));
 
 app.use("/api/auth", require("./routes/authRoutes"));
