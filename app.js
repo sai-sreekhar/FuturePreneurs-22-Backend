edfdce1e3a71b6bc180811d25b12c95069f7d242
Sai Sreekar <sai.sreekhar@gmail.com>
2022-09-10 17:57:30 +0530

    modified cors

--- app.js
+++ app.js
@@ -8,13 +8,44 @@ const morgan = require("morgan");
 const app = express();
 app.use(express.json());
 
+// app.use(
+//   limiter({
+//     windowMs: 1 * 60 * 1000, //750 per min requests allowed from one IP address
+//     max: 750,
+//     message: {
+//       code: 429,
+//       message: "Too many requests made, please try again later",
+//     },
+//   })
+// );
 
-app.use(
-   cors({
-     origin: "*",
-     credentials: true,
-   })
- );
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader("Access-Control-Allow-Headers", "*");
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+// app.use(
+//   cors({
+//     origin: "*",
+//     credentials: true,
+//   })
+// );
 
 app.use(morgan("dev"));
 
