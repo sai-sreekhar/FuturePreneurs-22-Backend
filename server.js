8c78444db2c9b7904d4aab6c64510c9fdddef940
Sai Sreekar <sai.sreekhar@gmail.com>
2022-09-11 17:12:54 +0530

    modified pool size

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ console.log(path.resolve(__dirname, "./.env"));
 
 mongoose.connect(
   process.env.DB_CONNECTION,
-  { useNewUrlParser: true, maxPoolSize: 30, minPoolSize: 10 },
+  { useNewUrlParser: true, maxPoolSize: 200, minPoolSize: 100 },
   () => {
     console.log("Connected to DataBase");
   }
