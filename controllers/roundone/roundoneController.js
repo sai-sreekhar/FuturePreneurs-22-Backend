cbd340bea3efed8ecef0fc44ccc7ab9db7752d94
Sai Sreekar <sai.sreekhar@gmail.com>
2022-10-15 23:00:24 +0530

    added get map route

--- controllers/roundone/roundoneController.js
+++ controllers/roundone/roundoneController.js
@@ -5,117 +5,6 @@ const Team = require("../../models/teamModel");
 const { errorCodes, objectIdLength, maps } = require("../../utils/constants");
 const { roundOneValidationMapSchema } = require("./validationSchema");
 
-// exports.getOptions = catchAsync(async (req, res, next) => {
-//     if (req.params.teamId.length !== objectIdLength) {
-//         return next(
-//             new AppError("Invalid TeamId", 412, errorCodes.INVALID_TEAM_ID)
-//         );
-//     }
-
-//     const team = await Team.findOne({ _id: req.params.teamId });
-
-//     if (!team) {
-//         return next(
-//             new AppError("Invalid TeamId", 412, errorCodes.INVALID_TEAM_ID)
-//         );
-//     }
-
-//     if (team.teamLeaderId.toString() !== req.user._id) {
-//         return next(
-//             new AppError(
-//                 "User doesn't belong to the Team or User isn't a Leader",
-//                 412,
-//                 errorCodes.INVALID_USERID_FOR_TEAMID_OR_USER_NOT_LEADER
-//             )
-//         );
-//     }
-
-//     let roundOne = await RoundOneModel.findOne({ teamId: req.params.teamId });
-//     if (!roundOne) {
-//         roundOne = await new RoundOneModel({
-//             teamId: req.params.teamId,
-//             startTime: Date.now(),
-//             score: 0,
-//         }).save();
-//     }
-
-//     res.status(201).json({
-//         message: "Get Options Success",
-//         optionOne: "Young",
-//         optionTwo: "Old",
-//         optionThree: "Business"
-
-//     });
-// });
-
-// exports.submitOption = catchAsync(async (req, res, next) => {
-
-//     console.log(req.body);
-//     const { error } = roundOneValidationCategorySchema(req.body);
-//     if (error) {
-//         return next(
-//             new AppError(
-//                 error.details[0].message,
-//                 400,
-//                 errorCodes.INPUT_PARAMS_INVALID
-//             )
-//         );
-//     }
-
-//     if (req.params.teamId.length !== objectIdLength) {
-//         return next(
-//             new AppError("Invalid TeamId", 412, errorCodes.INVALID_TEAM_ID)
-//         );
-//     }
-
-//     const team = await Team.findOne({ _id: req.params.teamId });
-
-//     if (!team) {
-//         return next(
-//             new AppError("Invalid TeamId", 412, errorCodes.INVALID_TEAM_ID)
-//         );
-//     }
-
-//     if (team.teamLeaderId.toString() !== req.user._id) {
-//         return next(
-//             new AppError(
-//                 "User doesn't belong to the Team or User isn't a Leader",
-//                 412,
-//                 errorCodes.INVALID_USERID_FOR_TEAMID_OR_USER_NOT_LEADER
-//             )
-//         );
-//     }
-
-//     let roundOne = await RoundOneModel.findOne({ teamId: req.params.teamId });
-//     if (!roundOne) {
-//         return next(
-//             new AppError(
-//                 "User doesn't belong to the Team or User isn't a Leader",
-//                 412,
-//                 errorCodes.ROUNDONE_RESPONSE_BEFORE_START,
-
-//             )
-//         );
-
-//     }
-
-//     if (roundOne) {
-//         await RoundOneModel.findOneAndUpdate(
-//             {
-//                 teamId: req.params.teamId,
-//             },
-//             {
-//                 $set: { "categoryChoice": req.body.categoryChoice },
-//             }
-//         );
-//     }
-
-//     res.status(201).json({
-//         message: "Category Choice Saved: Success",
-//     })
-
-// });
-
 exports.startRoundOne = catchAsync(async (req, res, next) => {
   if (req.params.teamId.length !== objectIdLength) {
     return next(
@@ -236,7 +125,7 @@ exports.submitSelection = catchAsync(async (req, res, next) => {
       )
     );
   }
-  
+
   if (roundOne.endTime < Date.now()) {
     await RoundOneModel.findOneAndUpdate(
       {
@@ -250,8 +139,6 @@ exports.submitSelection = catchAsync(async (req, res, next) => {
       new AppError("Time Limit Reached", 412, errorCodes.TIME_LIMIT_REACHED)
     );
   }
-
-  
 
   const mapChoice = req.body.mapChoice;
   if (
@@ -286,5 +173,57 @@ exports.submitSelection = catchAsync(async (req, res, next) => {
 
   res.status(201).json({
     message: "Map Choice Saved Sucessfully",
+  });
+});
+
+exports.getMap = catchAsync(async (req, res, next) => {
+  if (req.params.teamId.length !== objectIdLength) {
+    return next(
+      new AppError("Invalid TeamId", 412, errorCodes.INVALID_TEAM_ID)
+    );
+  }
+
+  const team = await Team.findOne({ _id: req.params.teamId });
+
+  if (!team) {
+    return next(
+      new AppError("Invalid TeamId", 412, errorCodes.INVALID_TEAM_ID)
+    );
+  }
+
+  if (team.teamLeaderId.toString() !== req.user._id) {
+    return next(
+      new AppError(
+        "User doesn't belong to the Team or User isn't a Leader",
+        412,
+        errorCodes.INVALID_USERID_FOR_TEAMID_OR_USER_NOT_LEADER
+      )
+    );
+  }
+
+  if (!team.isTeamQualified) {
+    return next(
+      new AppError("Team is not qualified", 412, errorCodes.TEAM_NOT_QUALIFIED)
+    );
+  }
+
+  let roundOne = await RoundOneModel.findOne({ teamId: req.params.teamId });
+  if (!roundOne) {
+    return next(
+      new AppError(
+        "Round One Document Not Found",
+        412,
+        errorCodes.ROUND_ONE_NOT_DOUND
+      )
+    );
+  }
+
+  roundOne = await RoundOneModel.findOne({
+    teamId: req.params.teamId,
+  });
+
+  res.status(201).json({
+    message: "Map Choice Saved Sucessfully",
+    mapChoice: roundOne.finalMapChoice,
   });
 });
--- routes/roundoneRoutes.js
+++ routes/roundoneRoutes.js
@@ -1,13 +1,10 @@
 const express = require("express");
-const roundoneRoutes = require("../controllers/roundone/roundoneController");
-const roundoneRouter = express.Router();
+const roundOneRoutes = require("../controllers/roundone/roundoneController");
+const roundOneRouter = express.Router();
 const auth = require("../middleware/authMiddleware");
 
-// roundoneRouter.route("/:teamId").get(auth, roundoneRoutes.getDetails);
-// roundoneRouter.route("/option/:teamId").post(auth, roundoneRoutes.submitOption)'
-roundoneRouter.route("/:teamId").post(auth, roundoneRoutes.submitSelection);
-roundoneRouter.route("/start/:teamId").post(auth, roundoneRoutes.startRoundOne);
-// teamRouter.route("/quiz/answers/:teamId").post(auth, teamController.getAnswers);
-// teamRouter.route("/quiz/score/:teamId").post(auth, teamController.getScore);
+roundOneRouter.route("/:teamId").post(auth, roundOneRoutes.submitSelection);
+roundOneRouter.route("/start/:teamId").post(auth, roundOneRoutes.startRoundOne);
+roundOneRouter.route("/:teamId").get(auth, roundOneRoutes.getMap);
 
-module.exports = roundoneRouter;
+module.exports = roundOneRouter;
